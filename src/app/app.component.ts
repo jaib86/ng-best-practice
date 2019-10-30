c5d42ee7dee1172c81268deabb617c1fae1d3296
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-10-30 05:45:59 +0530

    Scroll to top with route configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
+export class AppComponent implements OnInit {
+  constructor() {}
 
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.subscription = this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(() => window.scroll(0, 0));
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  ngOnInit() {}
 }
